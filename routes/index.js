dc972ab0443b35ad2b2a17df731699e1b5f66516
osboxes.org <laura.gallego.menendez@alumnos.upm.es>
2017-05-22 17:53:17 +0200

    Corrección errores

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/author', function(req, res, next) {
     res.render('author');
 });
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);
@@ -34,12 +34,12 @@ router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 router.get('/quizzes/randomplay',          quizController.randomplay);
 router.get('/quizzes/randomcheck/:quizId(\\d+)' , quizController.randomcheck);
 router.get('/quizzes/randomnomore',          quizController.randomnomore);
-=======
+//=======
 //Pagina de ayuda
 router.get('/help', function(req, res, next) {
     res.render('help');
 });
->>>>>>> practica51
+//>>>>>>> practica51
 
 
 module.exports = router;
