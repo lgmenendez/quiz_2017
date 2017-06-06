bd872a047b47d7464c1f54fd65d42d3d59fa9972
osboxes.org <laura.gallego.menendez@alumnos.upm.es>
2017-06-07 00:16:52 +0200

    P53

--- routes/index.js
+++ routes/index.js
@@ -47,8 +47,8 @@ router.get('/author', function (req, res, next) {
 });
 
 // Pagina de ayuda
-router.get('/ayuda', function(req, res, next) {
-    res.render('ayuda');
+router.get('/help', function(req, res, next) {
+    res.render('help');
 });
 
 // Pagina para jugar
@@ -139,7 +139,7 @@ router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
     tipController.adminOrAuthorRequired,
     tipController.destroy);
 
-
+// Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/new',                 quizController.new);
@@ -151,6 +151,9 @@ router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
+router.get('/quizzes/randomplay',          quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)' , quizController.randomcheck);
+router.get('/quizzes/randomnomore', quizController.randomnomore);
 
 
 module.exports = router;
